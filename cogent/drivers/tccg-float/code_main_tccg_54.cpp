ab795e2bb067ce958bce437d308be9bc43be4f50
Srinath Srinivasan <ssrini27@ncsu.edu>
2024-06-26 09:34:27 -0400

    change use before declaraiton

--- cogent/drivers/tccg-float/code_main_tccg_54.cpp
+++ cogent/drivers/tccg-float/code_main_tccg_54.cpp
@@ -13,6 +13,29 @@
 // # ad-abc-dbc
 // # t3 [a,d] += sum(b,c) * t2 [a,b,c] * v2 [d,b,c];
 //
+
+// Initialize t3 (t3_temp), 9 t2 and 9 v2.
+void pre_Initializing_Input_Tensors(float *h_C, float *h_C_chk, int size_C, float *h_A, int size_A, float *h_B, int size_B)
+{
+    // t3
+    int i, j;
+    for (i = 0; i < size_C; i++)
+    {
+        h_C[i] = 0.0;
+        h_C_chk[i] = 0.0;
+    }
+
+    for (j = 0; j < size_A; j++)
+    {
+        h_A[j] = ((float)rand() / RAND_MAX);
+    }
+
+    for (j = 0; j < size_B; j++)
+    {
+        h_B[j] = ((float)rand() / RAND_MAX);
+    }
+}
+
 int main(int argc, char **argv)
 {
     // for sd2
@@ -83,26 +106,4 @@ int main(int argc, char **argv)
     free(host_B);
 
     return 0;
-}
-
-// Initialize t3 (t3_temp), 9 t2 and 9 v2.
-void pre_Initializing_Input_Tensors(float *h_C, float *h_C_chk, int size_C, float *h_A, int size_A, float *h_B, int size_B)
-{
-    // t3
-    int i, j;
-    for (i = 0; i < size_C; i++)
-    {
-        h_C[i] = 0.0;
-        h_C_chk[i] = 0.0;
-    }
-
-    for (j = 0; j < size_A; j++)
-    {
-        h_A[j] = ((float)rand() / RAND_MAX);
-    }
-
-    for (j = 0; j < size_B; j++)
-    {
-        h_B[j] = ((float)rand() / RAND_MAX);
-    }
 }
